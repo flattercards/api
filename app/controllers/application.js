e7574e31a751df7ddeff26cf008da3c9d29cc5c7
Adriaan van Rossum <git@adriaan.io>
2016-07-31 21:46:58 +0100

    Add cors

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,12 @@
 import { Controller } from 'lux-framework';
 
 class ApplicationController extends Controller {
-
+  beforeAction = [
+    function setCorsHeader(req, res) {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    }
+  ];
 }
 
 export default ApplicationController;
