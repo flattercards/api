1d95016353fcd26a59de245e0270673756e89dab
Adriaan <harianus@users.noreply.github.com>
2016-08-06 21:13:43 +0100

    Add route comments and only allow post requests on messages

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,18 @@
 export default function routes() {
-  this.resource('messages');
+
+  // Only allow posts on the messages routes
+  this.route('messages', {
+    method: 'POST',
+    action: 'create'
+  });
+
+  // the browser needs a preflight request for now
+  this.route('messages', {
+    method: 'OPTIONS',
+    action: 'preflight'
+  });
+
+  // Only allow index route for cards
   this.route('cards', {
     method: 'GET',
     action: 'index'
